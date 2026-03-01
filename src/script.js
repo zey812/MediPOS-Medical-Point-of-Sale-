08579b5440e1389ed0131e515236803a198a9bcc
farrasfaishal12030 <farras.faishal2006@gmail.com>
2026-03-01 12:23:47 +0700

    Menambahkan data dummy obat

---
+++ src/script.js
@@ -0,0 +1,8 @@
+const dataObat = [
+    { id: 1, nama: "Paracetamol", harga: 15000, stok: 50 },
+    { id: 2, nama: "Amoxicillin", harga: 25000, stok: 30 },
+    { id: 3, nama: "Vitamin C", harga: 10000, stok: 100 },
+    { id: 4, nama: "OBH Sirup", harga: 20000, stok: 20 },
+];
+
+console.log("Data Obat Berhasil Diload:", dataObat);
